6eed85ef44aaed297bfc06427f901064cd8905da
jai-youitv <jai.kumar@youi.tv>
2021-02-26 16:36:28 +0530

    Initial Commit
    Initial Commit

---
+++ youi/Project.cmake
@@ -0,0 +1,3 @@
+set(YI_PROJECT_NAME "RNVideoTester") # The name of the project. When building for Android, this is passed from Android Studio.
+set(YI_PACKAGE_NAME "tv.youi.rnvideotester") # The package name used for the project.")
+set(YI_DISPLAY_NAME "RNVideoTester") # The name of the application displayed on the device.
